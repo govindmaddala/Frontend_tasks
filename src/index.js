5cedb9863c776d2770e9b36bcbe0f0b95cb6287a
GOvind MADdala <99980734+govindmaddala@users.noreply.github.com>
2023-07-26 16:22:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 // import App3 from './App3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <HashRouter basename="/">
     <React.StrictMode>
       <App />
       {/* <App3 /> */}
     </React.StrictMode>
-  </Router>
+  </HashRouter>
 );
